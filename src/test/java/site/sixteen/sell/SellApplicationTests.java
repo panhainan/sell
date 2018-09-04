1928a7f1597ae37cbf688c9a67ff6861bf24af54
Panhainan <panhainan@yeah.net>
2018-09-05 00:26:09 +0800

    项目初步搭建

---
+++ src/test/java/site/sixteen/sell/SellApplicationTests.java
@@ -0,0 +1,26 @@
+package site.sixteen.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SellApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testLog(){
+        log.error("error...");
+        log.warn("warn...");
+        log.info("info...");
+        log.debug("debug...");
+    }
+
+}
